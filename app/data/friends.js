fe10f5387e5a14f1f16c99ea2a9dd78f6e74f46a
Broodis_Desktop <j.porter712@gmail.com>
2018-08-31 13:16:48 -0700

    html and friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendsDataArr = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Tim",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            5,
+            4,
+            1,
+            3,
+            2,
+            1,
+            2,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Daequan",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            5,
+            3,
+            1,
+            2,
+            4,
+            5,
+            3
+        ]
+    },
+]
+
+module.exports = friendsDataArr;
